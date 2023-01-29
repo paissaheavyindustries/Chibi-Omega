fc119619e7811a361102a8a33495d42271141ab7
Paissa Heavy Industries <kommandantkeen@gmail.com>
2023-01-29 16:39:57 +0200

    v1.0.0.0

--- Chibi Omega/Plugin.cs
+++ Chibi Omega/Plugin.cs
@@ -6,8 +6,7 @@ using Dalamud.Game.ClientState.Objects.Types;
 using CharacterStruct = FFXIVClientStructs.FFXIV.Client.Game.Character.Character;
 using GameObjectStruct = FFXIVClientStructs.FFXIV.Client.Game.Object.GameObject;
 using Dalamud.Game.Gui;
-using System.Xml.Serialization;
-using System;
+using Dalamud.Game.ClientState;
 
 namespace Chibi_Omega
 {
@@ -20,7 +19,8 @@ namespace Chibi_Omega
         private DalamudPluginInterface _pi { get; init; }
         private ObjectTable _ot { get; init; }
         private ChatGui _cg { get; init; }
-        private DateTime nextCheck = DateTime.Now.AddSeconds(1);
+        private ClientState _cs { get; init; }
+        private bool _lookingForOmega = false;
 
         [PluginService]
         public static SigScanner TargetModuleScanner { get; private set; }
@@ -28,18 +28,33 @@ namespace Chibi_Omega
         public Plugin(
             [RequiredVersion("1.0")] DalamudPluginInterface pluginInterface,
             [RequiredVersion("1.0")] ObjectTable objectTable,
+            [RequiredVersion("1.0")] ClientState clientState,
             [RequiredVersion("1.0")] ChatGui chatGui
         )
         {
             _pi = pluginInterface;
             _ot = objectTable;
             _cg = chatGui;
-            _pi.UiBuilder.Draw += DrawUI;
+            _cs = clientState;            
+            _cs.TerritoryChanged += _cs_TerritoryChanged;
+            _cs_TerritoryChanged(null, _cs.TerritoryType);
+        }
+
+        private void _cs_TerritoryChanged(object sender, ushort e)
+        {
+            if (e == 800 || e == 804 || e == 1122)
+            {
+                StartLooking();
+            }
+            else
+            {
+                StopLooking();
+            }
         }
 
         public void Dispose()
         {
-            _pi.UiBuilder.Draw -= DrawUI;
+            StopLooking();
         }
 
         private void DrawUI()
@@ -47,13 +62,32 @@ namespace Chibi_Omega
             ManipulateOmegaChan();
         }
 
+        private void StartLooking()
+        {
+            lock (this)
+            {
+                if (_lookingForOmega == false)
+                {
+                    _pi.UiBuilder.Draw += DrawUI;
+                    _lookingForOmega = true;
+                }
+            }
+        }
+
+        private void StopLooking()
+        {
+            lock (this)
+            {
+                if (_lookingForOmega == true)
+                {
+                    _pi.UiBuilder.Draw -= DrawUI;
+                    _lookingForOmega = false;
+                }
+            }
+        }
+
         private unsafe void ManipulateOmegaChan()
         {
-            if (DateTime.Now < nextCheck)
-            {
-                return;
-            }
-            nextCheck = DateTime.Now.AddSeconds(1);
             foreach (GameObject go in _ot)
             {
                 if (go is Character)
